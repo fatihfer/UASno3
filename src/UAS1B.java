fbeaaf081a010aa50dd893a3536be1e8091b7b91
fatihfer <fatihfernando01@gmail.com>
2023-01-17 12:57:04 +0700

    cek

--- src/UAS1B.java
+++ src/UAS1B.java
@@ -1,8 +1,31 @@
 public class UAS1B {
-private int ID;
-private int UserName;
-private int FullName;
+private String ID;
+private String UserName;
 private int posted;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(String fullName) {
+        FullName = fullName;
+    }
 
     public int getPosted() {
         return posted;
@@ -12,34 +35,28 @@ private int posted;
         this.posted = posted;
     }
 
-    public int getID() {
-        return ID;
-    }
+    private String FullName;
 
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-
-    public int getUserName() {
-        return UserName;
-    }
-
-    public void setUserName(int userName) {
-        UserName = userName;
-    }
-
-    public int getFullName() {
-        return FullName;
-    }
-
-    public void setFullName(int fullName) {
-        FullName = fullName;
-    }
     public UAS1B(){
-    }
-    public static void ShowNumberofPost(){
 
     }
+    public static void ShowNumberOfPost(){
 
+    }
+    public static void main(String[] args) {
+        int jumPost = 4;
+        Scanner sc = new Scanner(System.in);
+        UAS1B user1 =  new UAS1B();
+        user1.setID("001");
+        user1.setUserName("dbgb4a");
+        user1.setFullName("Debby Gibson");
+        ArrayList<String> myPost =  new ArrayList<>();
+        for(int index=0; index<jumPost;index++){
+            myPost.add(sc.nextLine());
+        }
+        user1.setPosted(myPost);
+        System.out.println("Total Posted : " + user1.ShowNumberOfPost());
+
+    }
 
 }
