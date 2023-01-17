55c3128226ca1879ef887e158071188470c1c718
fatihfer <fatihfernando01@gmail.com>
2023-01-17 12:46:12 +0700

    cek

---
+++ src/UAS1B.java
@@ -0,0 +1,45 @@
+public class UAS1B {
+private int ID;
+private int UserName;
+private int FullName;
+private int posted;
+
+    public int getPosted() {
+        return posted;
+    }
+
+    public void setPosted(int posted) {
+        this.posted = posted;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(int userName) {
+        UserName = userName;
+    }
+
+    public int getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(int fullName) {
+        FullName = fullName;
+    }
+    public UAS1B(){
+    }
+    public static void ShowNumberofPost(){
+
+    }
+
+
+}
